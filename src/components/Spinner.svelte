d3afe346f7a6ebe3a41b7391b432f05bf70ab0cc
MaxiCorrea <jmaxicorrea@gmail.com>
2021-07-16 21:59:24 -0300

    Create Spinner

---
+++ src/components/Spinner.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Jumper } from "svelte-loading-spinners";
+</script>
+
+<div>
+  <Jumper size="100" color="#FF3E00" unit="px" duration="1s" />
+</div>
+
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
